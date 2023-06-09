6f1f5a3d360c0cbb64bc5f709c402e163090f316
zakifahmi <73203819+zakifahmii@users.noreply.github.com>
2023-06-09 20:21:29 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" href="img/Logo Tarnus 1.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/index.css">
+    <title>TarunaNusantara</title>
+</head>
+
+<body>
+    <header>
+        <div class="head">
+            <p>SELAMAT DATANG DI WEBSITE SMA TARUNA NUSANTARA</p>
+        </div>
+        <div class="head">
+            <p>HOTLINE KAMI 0293 364195 (SMATN), 021<br>31903410 (LPTTN), 08119056172 (LPTTN)</p>
+        </div>
+        <div class="head">
+            <p>SENIN - SABTU 07:00 - 14:30, JUMAT 07:00 - 11:15</p>
+        </div>
+        <p></p>
+    </header>
+    <nav>
+        <div class="logo">
+            <a href="index.php">
+                <img src="img/Logo Tarnus 2.png">
+            </a>
+        </div>
+        <div class="link-nav">
+            <ul>
+                <div class="menu">
+                    <li><a href="index.php">Beranda</a></li>
+                    <li><a href="#tentang">Tentang</a></li>
+                    <li><a href="#berita">Berita</a></li>
+                    <li><a href="https://tarunanusantara.sch.id/blog/penerimaan-siswa-baru-t-a-2023-2024">Daftar</a></li>
+                </div>
+            </ul>
+        </div>
+    </nav>
+    <div class="main">
+        <video autoplay muted loop>
+            <source src="video/Video Tarnus.mp4" type="video/mp4">
+        </video>
+        <h3 class="action" id="tentang">TENTANG</h3>
+        <div class="row">
+            <div class="about">
+                <p>Ide pembuatan sekolah ini dicetuskan oleh <b>Menteri Pertahanan dan Keamanan (MENHANKAM PANGAB)</b> saat itu, <b>Jenderal TNI L.B. Moerdani</b> pada tanggal 20 Mei 1985
+                    di <b>Pendopo Agung Taman Siswa Yogyakarta</b>. Jenderal TNI L.B. Moerdani mempunyai visi untuk membangun sekolah yang mendidik manusia-manusia terbaik dari seluruh Indonesia
+                    dan menghasilkan lulusan yang dapat melanjutkan cita-cita para Proklamator.
+                </p>
+                <p>
+                    Untuk merealisasikan ide ini, maka dibuatlah MoU / nota kesepahaman antara ABRI dan Taman Siswa. Perguruan Taman Siswa dipilih karena merupakan organisasi kependidikan pertama di Indonesia.
+                    Dalam nota kesepahaman itu disepakati untuk membuat suatu lembaga bernama Lembaga Perguruan Taman Taruna Nusantara (LPTTN). Dengan Akta Notaris KOESBIONO SARMANHADI, S.H., M.H.
+                    Nomor : 116/Pdr/P/Not/1990/PN. Jaksel. Tanggal 24-02-1990, Nomor 21 Akta Notaris WENDA TAURUSITA AMIDJAJA, S.H. Nomor : C-204.Ht.03.05-TH.2004. Tanggal 23-07-2004 Lembaga ini merupakan kristalisasi
+                    dari visi Jenderal TNI L.B. Moerdani yang selanjutnya mengawasi proses pelaksanaan sekolah ini.
+                </p><br>
+                <p>
+                    <img src="https://lpttn.or.id/site/img/sejarah_old.jpg" alt="">
+                </p>
+                <p>
+                    <a href="https://tarunanusantara.sch.id/page/profile" style="color:#3771B6;">Lanjut Baca Artikel >></a>
+                </p>
+            </div>
+        </div>
+        <h3 class="action" id="berita">BERITA</h3>
+        <div class="row">
+            <div class="card">
+                <a href="https://tarunanusantara.sch.id/blog/m-riza-yudhistira-raih-bintang-garuda-tri-sakti-taruna-tama-emas">
+                    <img src="img/berita 1.jpeg">
+                </a>
+                <div class="isi">
+                    <h4><b>M. Riza Yudhistira raih Bintang Garuda Tri Sakti Taruna Tama Emas (Lulusan Terbaik)</b></h4>
+                    <p style="font-size: 12px;">08 MAY, 2023|By Admin</p>
+                </div>
+            </div>
+            <div class="card">
+                <a href="https://tarunanusantara.sch.id/blog/3-lagu-wajib-siswa-baru-saat-upacara-pembukaan-pendidikan">
+                    <img src="img/lagu wajib.jpg">
+                </a>
+                <div class="isi">
+                    <h4><b>3 Lagu Wajib Siswa Baru saat Upacara Pembukaan Pendidikan</b></h4>
+                    <p style="font-size: 12px;">02 JUL, 2022|By Admin</p>
+                </div>
+            </div>
+            <div class="card">
+                <a href="https://tarunanusantara.sch.id/blog/ceramah-letkol-%28tit-%29-deodatus-andreas-deddy-cahyadi-sunjoyo-ph-d-">
+                    <img src="img/LetkolDedi.jpg">
+                </a>
+                <div class="isi">
+                    <h4><b>Ceramah Letkol (Tit.) Deodatus Andreas Deddy Cahyadi Sunjoyo, Ph.D.</b></h4>
+                    <p style="font-size: 12px;">15 MAY, 2023|By Admin</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <a href="https://tarunanusantara.sch.id/blog" class="btn-berita">
+                LIHAT SEMUA BERITA
+            </a>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="row">
+            <div class="col-footer-1" id="footer">
+                <p>Jl. Raya Purworejo Km 5 Magelang - 56172</p>
+            </div>
+            <div class="col-footer-2" id="footer">
+                <p>Hotline:<br>0293 364195 (SMATN), 021 31903410 (LPTTN), 08119056172 (LPTTN)</p>
+                <p>Fax:<br>0293 364047 (SMA TN), 021 31906707 (LPTTN)</p>
+                <p>Email: <br>
+                    admin@tarunanusantara.sch.id<br>
+                    humas@tarunanusantara.sch.id<br>
+                    kesiswaan@tarunanusantara.sch.id</p>
+            </div>
+            <div class="col-footer-3" id="footer">
+                <a href="https://www.facebook.com/SMA.Taruna.Nusantara"><i class="fa fa-facebook"></i>&nbsp;&nbsp;FACEBOOK</a>
+                <a href="https://twitter.com/SMATN"><i class="fa fa-twitter"></i>&nbsp;&nbsp;TWITTER</a>
+                <a href="https://www.linkedin.com/school/sma-taruna-nusantara-magelang/"><i class="fa fa-linkedin"></i>&nbsp;&nbsp;LINKED IN</a>
+                <a href="https://www.instagram.com/sma.tarunanusantara/"><i class="fa fa-instagram"></i>&nbsp;&nbsp;INSTAGRAM</a>
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        <p>© Copyright 2023 | Taruna Nusantara</p>
+    </div>
+</body>
+
+</html>
